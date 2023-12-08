9496d2f82d8130f6e0a757ddf4a7645958d14a12
league <league@imac-24.attlocal.net>
2023-12-07 16:56:55 -0800

    i am on remarkable

---
+++ src/_03_if_else/_1_unbirthday/HappyBirthday.java
@@ -0,0 +1,95 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class HappyBirthday {
+	public static void main(String[] args) {
+	
+	String birthday = JOptionPane.showInputDialog("What is your birthday? mm/dd");
+	
+	
+	if(birthday.equals("10,7")) { 
+		JOptionPane.showMessageDialog(null,"happy birthday" );
+	
+	}
+	else {
+	
+		JOptionPane.showMessageDialog(null,"Happy unbirthday");
+	
+	
+	}
+	
+	
+	
+	
+	
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
